3ea1d08142de9f531d34171a1e44e0bb035ae18e
BironSu <bironsu@pursuit.org>
2018-10-05 16:47:42 -0400

    first commit

---
+++ xCodeWithAlan/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xCodeWithAlan
+//
+//  Created by Biron Su on 10/5/18.
+//  Copyright © 2018 Biron Su. All rights reserved.
+//
+
+import Foundation
+
+print("xCode with Alan")
+
